0ea5aaa9e664555fbdc5e12602b5648d1033f57d
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-03-16 18:20:17 +0330

    feat :printed 3 variable

--- src/com/company/Exp1BasicOpratorjava.java
+++ src/com/company/Exp1BasicOpratorjava.java
@@ -1,7 +1,10 @@
 package com.company;
 public class Exp1BasicOpratorjava {
-
     public static void main(String[] args) {
-
+        int a=50;
+        int b=60;
+        int c ;
+        c = a + b;
+        System.out.println("c="+c);
     }
 }
